ccba75ad94386ce080a6f7596870f5b6ff780627
abdel1touimi <abdel1.touimi@gmail.com>
2024-12-20 16:38:26 +0100

    keeping basic states

--- config/shared.lua
+++ config/shared.lua
@@ -4,15 +4,11 @@ local allStates = {}
 allStates.stress = require 'config.states.stress'
 allStates.hunger = require 'config.states.hunger'
 allStates.thirst = require 'config.states.thirst'
-allStates.thc = require 'config.states.thc'
-allStates.alcohol = require 'config.states.alcohol'
 allStates.painkiller = require 'config.states.painkiller'
 
 -- boolean
 allStates.dead = require 'config.states.dead'
-allStates.grab = require 'config.states.grab'
 allStates.cuff = require 'config.states.cuff'
-allStates.ziptie = require 'config.states.ziptie'
 
 -- custom
 allStates.escort = require 'config.states.escort'
